a2cacf043bc5014080710c79b34ce3f129b2a258
huynhduy <nlhd.2701@gmail.com>
2024-12-21 23:45:20 +0700

    d

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ dotenv.config();
 connectDB();
 const app = express();
 
-// const PORT = process.env.PORT || 5000;
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT || 5000; 
+const PORT = 10000; 
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
